bd9f28e8af64cb24448133a37f0c8db79699e195
Eleandro Duzentos <eleandro@inbox.ru>
2021-02-27 01:00:42 +0100

    Added music model

---
+++ lib/modules/music/model.dart
@@ -0,0 +1,13 @@
+class Music {
+  final String title;
+  final String artist;
+  final String? album;
+  final String art;
+
+  Music({
+    required this.title,
+    required this.artist,
+    this.album,
+    required this.art,
+  });
+}
