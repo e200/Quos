f93b8ead448cda4ede80d89124ae3edea42bbc76
Eleandro Duzentos <eleandro@inbox.ru>
2021-02-27 01:00:48 +0100

    Added theme.dart

---
+++ lib/theme.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+final appGradientColors = [
+  Color(0xFF313251),
+  Color(0xFF171826),
+];
+final mutedTextColor = Color(0xFF9CB1EA);
+
+final appBorderRadius = 10.0;
+
+ThemeData getTheme(BuildContext context) {
+  return ThemeData.dark().copyWith(
+    primaryColor: Colors.deepPurple,
+    visualDensity: VisualDensity.adaptivePlatformDensity,
+  );
+}
