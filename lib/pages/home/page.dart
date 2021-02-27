fa6bdf08c55c1a9a0dd282f2a1cb421b501a04e9
Eleandro Duzentos <eleandro@inbox.ru>
2021-02-27 01:05:25 +0100

    Added HomePage

--- lib/pages/home/page.dart
+++ lib/pages/home/page.dart
@@ -1,3 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:quos/modules/music/model.dart';
+import 'package:quos/widgets/space.dart';
+
+import '../../theme.dart';
+
 final _musics = [
   Music(
     title: 'Malembe',
@@ -20,6 +26,117 @@ final _musics = [
     art: 'assets/album2.jpg',
   ),
 ];
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: QuosBackground(
+          child: SafeArea(
+        child: NestedScrollView(
+          headerSliverBuilder: (context, innerBoxIsScrolled) {
+            return [
+              SliverAppBar(
+                backgroundColor: Colors.transparent,
+                floating: true,
+                elevation: 0,
+                title: _HomePageTitle(),
+                actions: [
+                  PopupMenuButton(
+                    icon: Icon(Icons.more_vert),
+                    itemBuilder: (context) {
+                      return [
+                        PopupMenuItem(
+                          child: Text('Settings'),
+                        ),
+                        PopupMenuItem(
+                          child: Text('About'),
+                        ),
+                      ];
+                    },
+                  ),
+                ],
+              ),
+            ];
+          },
+          body: SingleChildScrollView(
+            physics: BouncingScrollPhysics(),
+            child: Wrap(
+              runSpacing: 30,
+              children: [
+                QuosSection(
+                  title: Text('Recently played'),
+                  actions: [
+                    QuosAction(
+                      title: Text(
+                        'More',
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                      icon: Icon(
+                        Icons.chevron_right,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                  child: QuosMusicList(),
+                ),
+                QuosSection(
+                  title: Text('Mostly played'),
+                  actions: [
+                    QuosAction(
+                      title: Text(
+                        'More',
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                      icon: Icon(
+                        Icons.chevron_right,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                  child: QuosMusicList(),
+                ),
+                QuosSection(
+                  title: Text('Albums'),
+                  actions: [
+                    QuosAction(
+                      title: Text(
+                        'More',
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                      icon: Icon(
+                        Icons.chevron_right,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                  child: QuosMusicList(),
+                ),
+                QuosSection(
+                  title: Text('Genres'),
+                  actions: [
+                    QuosAction(
+                      title: Text(
+                        'More',
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                      icon: Icon(
+                        Icons.chevron_right,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                  child: QuosMusicList(),
+                ),
+                SpaceY(),
+              ],
+            ),
+          ),
+        ),
+      )),
+    );
+  }
+}
 
 class _HomePageTitle extends StatelessWidget {
   const _HomePageTitle({
