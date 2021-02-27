b548f20d5022587ba2e34332ca59b9dbc40fb40e
Eleandro Duzentos <eleandro@inbox.ru>
2021-02-27 01:02:53 +0100

    Added QuosSection

--- lib/pages/home/page.dart
+++ lib/pages/home/page.dart
@@ -1,4 +1,42 @@
 
+class QuosSection extends StatelessWidget {
+  final Widget? title;
+  final List<Widget> actions;
+  final Widget child;
+
+  const QuosSection({
+    Key? key,
+    this.title,
+    this.actions = const [],
+    required this.child,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      runSpacing: 15,
+      children: [
+        if (title != null) ...[
+          DefaultTextStyle(
+            style: TextStyle(fontSize: 20),
+            child: Padding(
+              padding: const EdgeInsets.only(left: 15),
+              child: Row(
+                children: [
+                  title!,
+                  Spacer(),
+                  if (actions.isNotEmpty) ...actions,
+                ],
+              ),
+            ),
+          ),
+        ],
+        child,
+      ],
+    );
+  }
+}
+
 class QuosBackground extends StatelessWidget {
   final Widget child;
 
