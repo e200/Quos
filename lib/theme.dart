916771579dc8cd39cd1905081b9e3e351f5da94c
Eleandro Duzentos <eleandro@inbox.ru>
2021-02-28 15:30:06 +0100

    Added app colors

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,23 +1,22 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 
 final appGradientColors = [
-  Color(0xFF313251),
-  Color(0xFF171826),
+  const Color(0xFF313251),
+  const Color(0xFF171826),
 ];
-final mutedTextColor = Color(0xFF9CB1EA);
 
-final appBorderRadius = 10.0;
+const iconButtonColor = Color(0xFFA785EE);
+
+const mutedTextColor = Color(0xFF9CB1EA);
+
+const appBorderRadius = 15.0;
 
 ThemeData getTheme(BuildContext context) {
   return ThemeData.dark().copyWith(
     primaryColor: Colors.deepPurple,
     visualDensity: VisualDensity.adaptivePlatformDensity,
-    pageTransitionsTheme: PageTransitionsTheme(
-      builders: {
-        TargetPlatform.android: CupertinoPageTransitionsBuilder(),
-      }
-    ),
+    pageTransitionsTheme: const PageTransitionsTheme(builders: {
+      TargetPlatform.android: CupertinoPageTransitionsBuilder(),
+    }),
   );
 }
