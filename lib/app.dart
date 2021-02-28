975fedd1f7a1094fda7588314a6e6f5a30634d29
Eleandro Duzentos <eleandro@inbox.ru>
2021-02-28 15:29:57 +0100

    Changed SystemChrome colors

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:quos/pages/home/page.dart';
 import 'package:quos/theme.dart';
 
-class QuosApp extends StatelessWidget {
+class QuosApp extends StatefulWidget {
+  @override
+  _QuosAppState createState() => _QuosAppState();
+}
+
+class _QuosAppState extends State<QuosApp> with WidgetsBindingObserver {
+  @override
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance!.addObserver(this);
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.resumed) {
+      _setSystemOverlayStyle();
+    }
+  }
+
+  void _setSystemOverlayStyle() {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      systemNavigationBarColor: appGradientColors.last,
+      systemNavigationBarIconBrightness: Brightness.light,
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
+    _setSystemOverlayStyle();
+    
     return MaterialApp(
       title: 'QuosApp',
       theme: getTheme(context),
