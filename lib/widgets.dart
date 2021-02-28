7369fb1cc3311979722757ef3ef3a5d5bc460ee4
Eleandro Duzentos <eleandro@inbox.ru>
2021-02-28 15:30:36 +0100

    Added QuosMusicArt

--- lib/widgets.dart
+++ lib/widgets.dart
@@ -47,6 +47,58 @@ class QuosBackground extends StatelessWidget {
   }
 }
 
+class QuosMusicArt extends StatelessWidget {
+  final Music music;
+  final BorderRadius? borderRadius;
+
+  const QuosMusicArt({
+    Key? key,
+    this.borderRadius,
+    required this.music,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: borderRadius ?? BorderRadius.circular(appBorderRadius),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(.2),
+            blurRadius: 5,
+            offset: const Offset(0, 5),
+          ),
+        ],
+      ),
+      child: ClipRRect(
+        borderRadius: borderRadius ?? BorderRadius.circular(appBorderRadius),
+        child: Stack(
+          children: [
+            Image.asset(
+              music.art,
+              fit: BoxFit.cover,
+            ),
+            /* Positioned.fill(
+              child: Center(
+                child: Container(
+                  padding: EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    color: Colors.black.withOpacity(.3),
+                    borderRadius: BorderRadius.circular(100),
+                  ),
+                  child: Icon(
+                    Icons.play_arrow,
+                    size: 14,
+                  ),
+                ),
+              ),
+            ), */
+          ],
+        ),
+      ),
+    );
+  }
+}
 class SpaceY extends StatelessWidget {
   final double height;
 
