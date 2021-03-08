9eaad45d29fd2d81068e52a6a7c359efa784ff68
Eleandro Duzentos <eleandro@inbox.ru>
2021-03-08 23:59:49 +0100

    feat: Added ProviderScope

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+// @dart=2.9
+
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:quos/app.dart';
 
 void main() {
   Paint.enableDithering = true;
-  
-  runApp(QuosApp());
+
+  runApp(ProviderScope(child: QuosApp()));
 }
