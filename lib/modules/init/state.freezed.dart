3a41052770faeb19f765a8ee75ab2e65c39a6044
Eleandro Duzentos <eleandro@inbox.ru>
2021-03-10 21:01:10 +0100

    feature: Added InitState

---
+++ lib/modules/init/state.freezed.dart
@@ -0,0 +1,344 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides
+
+part of 'state.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more informations: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+class _$InitStateTearOff {
+  const _$InitStateTearOff();
+
+  _Initializing initializing() {
+    return const _Initializing();
+  }
+
+  _Initialized initialized() {
+    return const _Initialized();
+  }
+
+  _Error error() {
+    return const _Error();
+  }
+}
+
+/// @nodoc
+const $InitState = _$InitStateTearOff();
+
+/// @nodoc
+mixin _$InitState {
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() initializing,
+    required TResult Function() initialized,
+    required TResult Function() error,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? initializing,
+    TResult Function()? initialized,
+    TResult Function()? error,
+    required TResult orElse(),
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(_Initializing value) initializing,
+    required TResult Function(_Initialized value) initialized,
+    required TResult Function(_Error value) error,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(_Initializing value)? initializing,
+    TResult Function(_Initialized value)? initialized,
+    TResult Function(_Error value)? error,
+    required TResult orElse(),
+  }) =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $InitStateCopyWith<$Res> {
+  factory $InitStateCopyWith(InitState value, $Res Function(InitState) then) =
+      _$InitStateCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class _$InitStateCopyWithImpl<$Res> implements $InitStateCopyWith<$Res> {
+  _$InitStateCopyWithImpl(this._value, this._then);
+
+  final InitState _value;
+  // ignore: unused_field
+  final $Res Function(InitState) _then;
+}
+
+/// @nodoc
+abstract class _$InitializingCopyWith<$Res> {
+  factory _$InitializingCopyWith(
+          _Initializing value, $Res Function(_Initializing) then) =
+      __$InitializingCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$InitializingCopyWithImpl<$Res> extends _$InitStateCopyWithImpl<$Res>
+    implements _$InitializingCopyWith<$Res> {
+  __$InitializingCopyWithImpl(
+      _Initializing _value, $Res Function(_Initializing) _then)
+      : super(_value, (v) => _then(v as _Initializing));
+
+  @override
+  _Initializing get _value => super._value as _Initializing;
+}
+
+/// @nodoc
+class _$_Initializing implements _Initializing {
+  const _$_Initializing();
+
+  @override
+  String toString() {
+    return 'InitState.initializing()';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) || (other is _Initializing);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() initializing,
+    required TResult Function() initialized,
+    required TResult Function() error,
+  }) {
+    return initializing();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? initializing,
+    TResult Function()? initialized,
+    TResult Function()? error,
+    required TResult orElse(),
+  }) {
+    if (initializing != null) {
+      return initializing();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(_Initializing value) initializing,
+    required TResult Function(_Initialized value) initialized,
+    required TResult Function(_Error value) error,
+  }) {
+    return initializing(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(_Initializing value)? initializing,
+    TResult Function(_Initialized value)? initialized,
+    TResult Function(_Error value)? error,
+    required TResult orElse(),
+  }) {
+    if (initializing != null) {
+      return initializing(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class _Initializing implements InitState {
+  const factory _Initializing() = _$_Initializing;
+}
+
+/// @nodoc
+abstract class _$InitializedCopyWith<$Res> {
+  factory _$InitializedCopyWith(
+          _Initialized value, $Res Function(_Initialized) then) =
+      __$InitializedCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$InitializedCopyWithImpl<$Res> extends _$InitStateCopyWithImpl<$Res>
+    implements _$InitializedCopyWith<$Res> {
+  __$InitializedCopyWithImpl(
+      _Initialized _value, $Res Function(_Initialized) _then)
+      : super(_value, (v) => _then(v as _Initialized));
+
+  @override
+  _Initialized get _value => super._value as _Initialized;
+}
+
+/// @nodoc
+class _$_Initialized implements _Initialized {
+  const _$_Initialized();
+
+  @override
+  String toString() {
+    return 'InitState.initialized()';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) || (other is _Initialized);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() initializing,
+    required TResult Function() initialized,
+    required TResult Function() error,
+  }) {
+    return initialized();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? initializing,
+    TResult Function()? initialized,
+    TResult Function()? error,
+    required TResult orElse(),
+  }) {
+    if (initialized != null) {
+      return initialized();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(_Initializing value) initializing,
+    required TResult Function(_Initialized value) initialized,
+    required TResult Function(_Error value) error,
+  }) {
+    return initialized(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(_Initializing value)? initializing,
+    TResult Function(_Initialized value)? initialized,
+    TResult Function(_Error value)? error,
+    required TResult orElse(),
+  }) {
+    if (initialized != null) {
+      return initialized(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class _Initialized implements InitState {
+  const factory _Initialized() = _$_Initialized;
+}
+
+/// @nodoc
+abstract class _$ErrorCopyWith<$Res> {
+  factory _$ErrorCopyWith(_Error value, $Res Function(_Error) then) =
+      __$ErrorCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$ErrorCopyWithImpl<$Res> extends _$InitStateCopyWithImpl<$Res>
+    implements _$ErrorCopyWith<$Res> {
+  __$ErrorCopyWithImpl(_Error _value, $Res Function(_Error) _then)
+      : super(_value, (v) => _then(v as _Error));
+
+  @override
+  _Error get _value => super._value as _Error;
+}
+
+/// @nodoc
+class _$_Error implements _Error {
+  const _$_Error();
+
+  @override
+  String toString() {
+    return 'InitState.error()';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) || (other is _Error);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() initializing,
+    required TResult Function() initialized,
+    required TResult Function() error,
+  }) {
+    return error();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? initializing,
+    TResult Function()? initialized,
+    TResult Function()? error,
+    required TResult orElse(),
+  }) {
+    if (error != null) {
+      return error();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(_Initializing value) initializing,
+    required TResult Function(_Initialized value) initialized,
+    required TResult Function(_Error value) error,
+  }) {
+    return error(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(_Initializing value)? initializing,
+    TResult Function(_Initialized value)? initialized,
+    TResult Function(_Error value)? error,
+    required TResult orElse(),
+  }) {
+    if (error != null) {
+      return error(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class _Error implements InitState {
+  const factory _Error() = _$_Error;
+}
